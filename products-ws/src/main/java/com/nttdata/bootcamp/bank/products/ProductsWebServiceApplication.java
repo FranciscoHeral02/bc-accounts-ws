a8ee78a314614cb9aeb59683087bc6e519472a14
Francisco Hernandez <francisco.heral02@gmail.com>
2022-05-04 10:23:35 -0500

    Creacion del proyecto products

---
+++ products-ws/src/main/java/com/nttdata/bootcamp/bank/products/ProductsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.bank.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsWebServiceApplication.class, args);
+	}
+
+}
---
+++ products-ws/src/test/java/com/nttdata/bootcamp/bank/products/ProductsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.bank.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
